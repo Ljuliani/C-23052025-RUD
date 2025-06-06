b2babda6f3dd4feb49bd6ac9c2b6d89b567ace2a
Luciano <maxlucho1@gmail.com>
2025-06-06 17:16:29 -0300

    Añadir validaciones y mejorar formularios de alumno
    Se ha añadido un campo de validación para el DNI del alumno en `Create.cshtml`.
    Se ha agregado una nueva columna "For do" en la tabla de `Index.cshtml`.
    Se ha modificado el campo de duración de la carrera en `Create.cshtml`, eliminando el tipo `number` y añadiendo validación.
    El campo `Duracionaños` en `Carreras.cs` ha cambiado a `int?`, permitiendo que sea opcional.

--- Pages/Models/Carreras.cs
+++ Pages/Models/Carreras.cs
@@ -10,7 +10,7 @@ namespace C_23052025_RUD.Models
         [Required(ErrorMessage = "The field of Duration (In ages) is empty.")]
         [Range(1, 100, ErrorMessage = "Duration must be between 1 and 100.")]
 
-        public int Duracionaños { get; set; }
+        public int? Duracionaños { get; set; }
         [Required(ErrorMessage = "The field of Title awarded is empty.")]
         public string? Titulootorgado { get; set; }
 
